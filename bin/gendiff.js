36baf67ce99cbbf6a54d162f082aee7cdc05d69a
yana_pfl <yanna.pvl24@gmail.com>
2023-07-21 15:47:16 +0300

    add gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format[type]','output format (default: "stylish")')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
+
+    program.parse();
