2ee8725e55a8281fa11a2d11f36e3f0a25f52943
yana_pfl <yanna.pvl24@gmail.com>
2023-08-03 12:45:10 +0300

    add file1.json file2.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,6 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .option('-V, --version', 'output the version number')
     .option('-f, --format[type]','output format (default: "stylish")')
     .argument('<filepath1>')
     .argument('<filepath2>');
