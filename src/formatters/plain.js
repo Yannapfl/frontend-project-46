aec7c95887bb4efa029fcc2527fb45e7f897c94a
yana_pfl <yanna.pvl24@gmail.com>
2023-10-04 23:33:00 +0300

    refactoring 4

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,27 +11,29 @@ const stringify = (value) => {
   return value;
 };
 
-const getPlain = (arr, path = '') => {
-  const path1 = path;
-  const plainedArr = arr.map((item) => {
-    switch (item.type) {
-      case 'nested':
-        return getPlain(item.children, (`${path}${item.key}.`));
-      case 'equal':
-        return null;
-      case 'added':
-        return `Property '${path1}${item.key}' was added with value: ${stringify(item.value)}`;
-      case 'deleted':
-        return `Property '${path}${item.key}' was removed`;
-      case 'updated':
-        return `Property '${path}${item.key}' was updated. From ${stringify(item.value1)} to ${stringify(item.value2)}`;
-      default:
-        throw new Error(`Unknown ${item.type}.`);
-    }
-  });
-  const filteredArr = plainedArr.filter((item) => item !== null);
-  const text = filteredArr.join('\n');
-  return text;
+const getPlain = (data) => {
+  const plain = (content, path = '') => {
+    const plainedArr = content.map((item) => {
+      switch (item.type) {
+        case 'nested':
+          return plain(item.children, (`${path}${item.key}.`));
+        case 'equal':
+          return null;
+        case 'added':
+          return `Property '${path}${item.key}' was added with value: ${stringify(item.value)}`;
+        case 'deleted':
+          return `Property '${path}${item.key}' was removed`;
+        case 'updated':
+          return `Property '${path}${item.key}' was updated. From ${stringify(item.value1)} to ${stringify(item.value2)}`;
+        default:
+          throw new Error(`Unknown ${item.type}.`);
+      }
+    });
+    const filteredArr = plainedArr.filter((item) => item !== null);
+    const text = filteredArr.join('\n');
+    return text;
+  };
+  return plain(data);
 };
 
 export default getPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,14 +2,14 @@ import _ from 'lodash';
 
 const countOpeningSpace = 4;
 const defendSpace = 2;
-const countInitialSpaces = (depth) => defendSpace + depth * countOpeningSpace;
-const countClosingSpaces = (depth) => depth * countOpeningSpace;
+const setInitialSpaces = (depth) => ' '.repeat(defendSpace + depth * countOpeningSpace);
+const setClosingSpaces = (depth) => ' '.repeat(depth * countOpeningSpace);
 
 const stringify = (value, depth) => {
   if (_.isObject(value)) {
     const keys = Object.keys(value);
-    const stringifyObject = keys.map((key) => `${' '.repeat(countInitialSpaces(depth + 1))}  ${key}: ${stringify(value[key], depth + 1)}`);
-    return `{\n${stringifyObject.join('\n')}\n${' '.repeat(countClosingSpaces(depth + 1))}}`;
+    const stringifyObject = keys.map((key) => `${(setInitialSpaces(depth + 1))}  ${key}: ${stringify(value[key], depth + 1)}`);
+    return `{\n${stringifyObject.join('\n')}\n${setClosingSpaces(depth + 1)}}`;
   }
   return value;
 };
@@ -19,17 +19,16 @@ const getStylish = (data) => {
     const stringifyData = content.map((item) => {
       switch (item.type) {
         case 'nested':
-          return [`${' '.repeat(countInitialSpaces(depth))}  ${item.key}: {\n${stylish(item.children, depth + 1)}`,
-            `${' '.repeat(countClosingSpaces(depth + 1))}}`].join('\n');
+          return `${setInitialSpaces(depth)}  ${item.key}: {\n${stylish(item.children, depth + 1)}\n${setClosingSpaces(depth + 1)}}`;
         case 'equal':
-          return `${' '.repeat(countInitialSpaces(depth))}  ${item.key}: ${stringify(item.value, depth)}`;
+          return `${setInitialSpaces(depth)}  ${item.key}: ${stringify(item.value, depth)}`;
         case 'added':
-          return `${' '.repeat(countInitialSpaces(depth))}+ ${item.key}: ${stringify(item.value, depth)}`;
+          return `${setInitialSpaces(depth)}+ ${item.key}: ${stringify(item.value, depth)}`;
         case 'deleted':
-          return `${' '.repeat(countInitialSpaces(depth))}- ${item.key}: ${stringify(item.value, depth)}`;
+          return `${setInitialSpaces(depth)}- ${item.key}: ${stringify(item.value, depth)}`;
         case 'updated':
-          return [`${' '.repeat(countInitialSpaces(depth))}- ${item.key}: ${stringify(item.value1, depth)}`,
-            `${' '.repeat(countInitialSpaces(depth))}+ ${item.key}: ${stringify(item.value2, depth)}`].join('\n');
+          return [`${setInitialSpaces(depth)}- ${item.key}: ${stringify(item.value1, depth)}`,
+            `${setInitialSpaces(depth)}+ ${item.key}: ${stringify(item.value2, depth)}`].join('\n');
         default:
           throw new Error(`'Unknown ${item.type}'`);
       }
